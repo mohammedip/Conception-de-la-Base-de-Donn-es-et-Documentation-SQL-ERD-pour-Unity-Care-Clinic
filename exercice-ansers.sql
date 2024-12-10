534e538cf9bf2c118216f1bcc792356a4205294e
Mohammed reida <reidamohammed3@gmail.com>
2024-12-10 15:23:13 +0100

    add the anser of the bonus questions in the exercice-ansers fille

--- exercice-ansers.sql
+++ exercice-ansers.sql
@@ -63,10 +63,7 @@ SELECT MAX(appointment_date)as later_date , MAX(appointment_time)as later_heure 
 
 14-
 
-SELECT SUM(admissions.ROWS) as admissions_number,(SELECT room_id from rooms WHERE admissions.room_id = rooms.room_id ) as room  from admissions GROUP BY room_id
-
-
-SELECT (SELECT room_id from rooms WHERE admissions.room_id = rooms.room_id ) as room, COUNT(*) as admissions_number from admissions GROUP BY 
+SELECT SUM(*) as admissions_number,(SELECT room_id from rooms WHERE admissions.room_id = rooms.room_id ) as room  from admissions GROUP BY room_id 
 
 15-
 
@@ -87,5 +84,29 @@ SELECT gender,COUNT(gender)as how_match_gerders  from patients GROUP BY gender H
 20-
 
 CREATE VIEW admissions_en_cours AS SELECT * from admissions WHERE admission_date>'2024-12-10'
-
 SELECT * from admissions_en_cours 
+
+Bonus : 
+16-
+
+SELECT patients.first_name as First_name, patients.last_name AS Last_name, appointments.appointment_date as appointment_date, appointments.appointment_time as appointment_time ,appointments.reason ,doctors.first_name as doctors_first_name,doctors.last_name as doctors_last_name FROM appointments JOIN patients on patients.patient_id=appointments.patient_id JOIN doctors on doctors.doctor_id=appointments.doctor_id
+
+1-
+
+SELECT patients.first_name , doctors.first_name from appointments  JOIN patients on appointments.patient_id=patients.patient_id  JOIN doctors on appointments.doctor_id=doctors.doctor_id
+
+2-
+
+select appointment_id,appointment_date,appointment_time,reason , departments.department_name from doctors JOIN appointments on appointments.doctor_id=doctors.doctor_id JOIN departments on departments.department_id=doctors.department_id
+
+3-
+
+select  doctors.first_name,doctors.last_name,medications.medication_id,medications.medication_name,medications.dosage,prescriptions.dosage_instrufctions from prescriptions JOIN medications on medications.medication_id=prescriptions.medication_id JOIN doctors on doctors.doctor_id=prescriptions.doctor_id
+
+4-
+
+SELECT rooms.room_number as Room,patients.first_name as first_name,patients.last_name as last_name ,admissions.admission_date as Admission , admissions.discharge_date as Discharge FROM admissions JOIN rooms on admissions.room_id=rooms.room_id JOIN patients on patients.patient_id=admissions.patient_id
+
+5-
+
+SELECT 
